a7b42177654c65bf580ec943193b92afb91b75c8
Arthur Zheng <arthurzheng150@gmail.com>
2026-05-02 13:18:49 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# 8x8 Matrix Display
+
+![Version](https://img.shields.io/github/v/tag/openblock-plugin/eitghtTimesEightMatirxDisplay?label=version)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+![Type](https://img.shields.io/badge/type-extension-orange.svg)
+![Tags](https://img.shields.io/badge/tags-display-lightgrey.svg)
+
+8x8 matrix display module based on MAX7219.
+
+## Documentation
+
+For detailed usage instructions, please visit:
+
+**[OpenBlock Wiki](https://wiki.openblock.cc)**
---
+++ src/translations.js
@@ -0,0 +1,107 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections (e.g., "zh-cn", "zh-tw", "ja", etc.) should be
+ *   manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'eitghtTimesEightMatirxDisplay.name': '8x8 Matrix Display',
+            'eitghtTimesEightMatirxDisplay.description': '8x8 matrix display module based on MAX7219.'
+        },
+        'ru': {
+            'eitghtTimesEightMatirxDisplay.name': '8x8 Matrix Display',
+            'eitghtTimesEightMatirxDisplay.description': '8x8 matrix display module based on MAX7219.'
+        },
+        'zh-cn': {
+            'eitghtTimesEightMatirxDisplay.name': '8x8 Matrix Display',
+            'eitghtTimesEightMatirxDisplay.description': '8x8 matrix display module based on MAX7219.'
+        },
+        'zh-tw': {
+            'eitghtTimesEightMatirxDisplay.name': '8x8 Matrix Display',
+            'eitghtTimesEightMatirxDisplay.description': '8x8 matrix display module based on MAX7219.'
+        }
+    },
+    'extensions': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    },
+    'blocks': {
+        'en': {
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_CATEGORY': '8x8 Matrix Display',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_INIT': 'init 8x8 matrix display pin CS %1',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_INIT_TOOLTIP': 'pin DIN to MOSI and CLK to SCK.',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_DRAWPIXEL': '8x8 martix display draw pixel x: %1 y: %2 color %3',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_COLOR_RED': 'red',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_COLOR_BLACK': 'black',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_DRAWLINE': '8x8 martix display draw line x0: %1 y0: %2, x1: %3 y1: %4 color %5',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_SHOWCHAR': '8x8 martix display show char %1 color %2',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_SHOWNUMBER': '8x8 martix display show number %1 color %2',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_FILL': '8x8 martix display fill color %1',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_REFRESH': 'refresh the display of 8x8 martix display',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_SETBRIGHTNESS': '8x8 martix display set brightness %1',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_SETROTATION': '8x8 martix display set rotation %1'
+        },
+        'ru': {
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_CATEGORY': '8x8 Matrix Display',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_INIT': 'init 8x8 matrix display pin CS %1',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_INIT_TOOLTIP': 'pin DIN to MOSI and CLK to SCK.',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_DRAWPIXEL': '8x8 martix display draw pixel x: %1 y: %2 color %3',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_COLOR_RED': 'red',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_COLOR_BLACK': 'black',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_DRAWLINE': '8x8 martix display draw line x0: %1 y0: %2, x1: %3 y1: %4 color %5',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_SHOWCHAR': '8x8 martix display show char %1 color %2',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_SHOWNUMBER': '8x8 martix display show number %1 color %2',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_FILL': '8x8 martix display fill color %1',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_REFRESH': 'refresh the display of 8x8 martix display',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_SETBRIGHTNESS': '8x8 martix display set brightness %1',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_SETROTATION': '8x8 martix display set rotation %1'
+        },
+        'zh-cn': {
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_CATEGORY': '8x8 Matrix Display',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_INIT': 'init 8x8 matrix display pin CS %1',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_INIT_TOOLTIP': 'pin DIN to MOSI and CLK to SCK.',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_DRAWPIXEL': '8x8 martix display draw pixel x: %1 y: %2 color %3',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_COLOR_RED': 'red',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_COLOR_BLACK': 'black',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_DRAWLINE': '8x8 martix display draw line x0: %1 y0: %2, x1: %3 y1: %4 color %5',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_SHOWCHAR': '8x8 martix display show char %1 color %2',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_SHOWNUMBER': '8x8 martix display show number %1 color %2',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_FILL': '8x8 martix display fill color %1',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_REFRESH': 'refresh the display of 8x8 martix display',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_SETBRIGHTNESS': '8x8 martix display set brightness %1',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_SETROTATION': '8x8 martix display set rotation %1'
+        },
+        'zh-tw': {
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_CATEGORY': '8x8 Matrix Display',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_INIT': 'init 8x8 matrix display pin CS %1',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_INIT_TOOLTIP': 'pin DIN to MOSI and CLK to SCK.',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_DRAWPIXEL': '8x8 martix display draw pixel x: %1 y: %2 color %3',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_COLOR_RED': 'red',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_COLOR_BLACK': 'black',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_DRAWLINE': '8x8 martix display draw line x0: %1 y0: %2, x1: %3 y1: %4 color %5',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_SHOWCHAR': '8x8 martix display show char %1 color %2',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_SHOWNUMBER': '8x8 martix display show number %1 color %2',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_FILL': '8x8 martix display fill color %1',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_REFRESH': 'refresh the display of 8x8 martix display',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_SETBRIGHTNESS': '8x8 martix display set brightness %1',
+            'EITGHTTIMESEIGHTMATIRXDISPLAY_SETROTATION': '8x8 martix display set rotation %1'
+        }
+    }
+};
